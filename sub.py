0ad3bcc1e64b52ac3a04792b570f79cf4c774a59
alexjjgreen <alexjjgreen@gmail.com>
2021-04-15 19:16:49 +0200

    full excel output

--- sub.py
+++ sub.py
@@ -4,17 +4,20 @@ import pandas as pd
 import shutil
 from ordered_set import OrderedSet
 import xlsxwriter
+from re import search
+
+#### --- GLOBALS --- ####
 
 doc = pp.PdfFileReader("Story Analysis WTD INTERNATIONAL.pdf")
-
 total_pages = doc.getNumPages()
-
 store_datasets = []
-
 story_datasets = {}
+# cat_datasets = {}
 
+# week to date counter, on false ignore pages, append current story_dataset to store_dataset
 wtd = True
 
+# mutate str to float on numeric cols, nb. order of ops prevents refactor, don't touch!
 def parse_to_numeric(filtered_text):
     parsed_text = []
     for i in filtered_text:
@@ -27,14 +30,15 @@ def parse_to_numeric(filtered_text):
         if "," in i:
             i = i.replace(",", "")
         if i == "n/a" or i == "N/A":
-            i = None
+            i = 0
         if (i != None) or (i != "Total"):
             float(i)
         parsed_text.append(i)
     
     return parsed_text
 
-for page in range(10):
+
+for page in range(total_pages):
     # get page extract text
     current_page = doc.getPage(page).extractText()
     # split into list
@@ -44,89 +48,84 @@ for page in range(10):
     for i in range(len(raw_text)):
         str(raw_text[i])
     
-    # global dataset obj
-    cat_datasets = []
-
     # check page type
     if raw_text[0] == "STORY ANALYSIS YESTERDAY":
         wtd = False
 
-        story_datasets["STORY"] = list(OrderedSet(story_datasets["STORY"]))
+        # check for 'dead' datasets
+        try:
+            story_datasets["STORY"] = list(OrderedSet(story_datasets["STORY"]))
 
-        for i in range(len(story_datasets["Unit Mix %"])):
-            if story_datasets["Item L3 Desc"][i] != "Total":
-                    story_datasets["STORY"].insert(i, story_datasets["STORY"][i -1])
-            if story_datasets["Unit Mix %"][i] == "n/a%":
-                story_datasets["Units"].insert(i, 0)
-            if i != 0:
-                story_datasets["store"].insert(i, story_datasets["store"][i - 1])
+            for i in range(len(story_datasets["Unit Mix %"])):
+                if story_datasets["Item L3 Desc"][i] != "Total":
+                        story_datasets["STORY"].insert(i, story_datasets["STORY"][i -1])
+                if story_datasets["Unit Mix %"][i] == "n/a%":
+                    story_datasets["Units"].insert(i, 0)
+                if i != 0:
+                    story_datasets["store"].insert(i, story_datasets["store"][i - 1])
+            
+            # parse Sales £, Units, Cash Mix %, Unit Mix % to float
+            story_datasets["Sales £"] = parse_to_numeric(story_datasets["Sales £"])
+            story_datasets["Cash Mix %"] = parse_to_numeric(story_datasets["Cash Mix %"])
+            story_datasets["Unit Mix %"] = parse_to_numeric(story_datasets["Unit Mix %"])
+            store_datasets.append(story_datasets)
+        except:
+            pass
 
-        # parse to numeric
-        #for i in range(4,8):
-        #    print(story_datasets[i])
-        #    print("------------------------------------------------")
-        #    if len(story_datasets[i]) > 0:
-        #        story_datasets[i] = parse_to_numeric(story_datasets[i])
-        
-        # story wtd finished, append to global store list
-
-        store_datasets.append(story_datasets)
         story_datasets = {}
 
     elif raw_text[0] == "STORY ANALYSIS WEEK TO DATE":
-        wtd = True
+        # check page is not empty
+        if len(raw_text) >= 9:
+            # set bool trigger for WTD pages without title
+            wtd = True
+            # get meta
+            story_datasets["store"] = [raw_text[1]]
+            del raw_text[0:2]
 
-        # get meta
-        story_datasets["store"] = [raw_text[1]]
-        del raw_text[0:2]
+            # get dict keys --> {Store: "", STORY: [], Item L3 Desc: [], Sales £: [], Units: [], Cash Mix %: [], Unit Mix %: []}
+            for i in range(6):
+                story_datasets[raw_text[i]] = []
+            
+            del raw_text[0:6]
 
-        # get dict keys --> {Store: "", STORY: [], Item L3 Desc: [], Sales £: [], Units: [], Cash Mix %: [], Unit Mix %: []}
-        for i in range(6):
-            story_datasets[raw_text[i]] = []
-        
-        del raw_text[0:6]
+            # strip story names into one col and append to datasets, break point on list i == Total && i + 1 == Total
+            c = 0
+            for i in range(len(raw_text)):
+                if (raw_text[i] == "Total") and (raw_text[i + 1] == "Total"):
+                    break
+                else:
+                    story_datasets["STORY"].append(raw_text[i])
+                    c += 1
 
-        # strip story names into one col and append to datasets, break point on list i == Total && i + 1 == Total
-        c = 0
-        for i in range(len(raw_text)):
-            if (raw_text[i] == "Total") and (raw_text[i + 1] == "Total"):
-                break
-            else:
-                story_datasets["STORY"].append(raw_text[i])
-                c += 1
+            del raw_text[0:c]
+            
+            # cash and units mix cols ALWAYS contain %, identify and /2 to get col length, strip and append to dict
+            col_len = int(sum("%" in s for s in raw_text)/2)
 
-        del raw_text[0:c]
-        
+            for i in range(len(raw_text) - col_len,len(raw_text)):
+                story_datasets["Unit Mix %"].append(raw_text[i])
+            del raw_text[len(raw_text) - col_len:]
 
-        # cash and units mix cols ALWAYS contain %, identify and /2 to get col length, strip and append to dict
+            for i in range(len(raw_text) - col_len,len(raw_text)):
+                story_datasets["Cash Mix %"].append(raw_text[i])
+            del raw_text[len(raw_text) - col_len:]
 
-        col_len = int(sum("%" in s for s in raw_text)/2)
+            # strip item desc append to Item L3 Desc
+            for i in range(0,col_len):
+                story_datasets["Item L3 Desc"].append(raw_text[i])
+            del raw_text[0:col_len]
 
-        for i in range(len(raw_text) - col_len,len(raw_text)):
-            story_datasets["Unit Mix %"].append(raw_text[i])
-        del raw_text[len(raw_text) - col_len:]
+            # strip Sales £, append and del
+            for i in range(0,col_len):
+                story_datasets["Sales £"].append(raw_text[i])
+            del raw_text[0:col_len]
 
-        for i in range(len(raw_text) - col_len,len(raw_text)):
-            story_datasets["Cash Mix %"].append(raw_text[i])
-        del raw_text[len(raw_text) - col_len:]
-
-        # strip item desc append to Item L3 Desc
-
-        for i in range(0,col_len):
-            story_datasets["Item L3 Desc"].append(raw_text[i])
-        del raw_text[0:col_len]
-
-        # strip Sales £, append and del
-        for i in range(0,col_len):
-            story_datasets["Sales £"].append(raw_text[i])
-        del raw_text[0:col_len]
-
-        #append last list to Units
-        for i in range(len(raw_text)):
-            story_datasets["Units"].append(raw_text[i])
-
-        #for key in story_datasets:
-        #    print("{} | {} \n".format(key,story_datasets[key]))
+            #append last list to Units
+            for i in range(len(raw_text)):
+                story_datasets["Units"].append(raw_text[i])
+        else:
+            pass
         
     elif (raw_text[0] == "STORY") and (wtd is True):
         # delete column titles
@@ -142,7 +141,6 @@ for page in range(10):
         del raw_text[0:story_appended_count]
 
         # cash and units mix cols ALWAYS contain %, identify and /2 to get col length, strip and append to dict
-
         col_len = int(sum("%" in s for s in raw_text)/2)
 
         for i in range(len(raw_text) - col_len,len(raw_text)):
@@ -154,7 +152,6 @@ for page in range(10):
         del raw_text[len(raw_text) - col_len:]
 
         # strip item desc append to Item L3 Desc
-
         for i in range(0,col_len):
             story_datasets["Item L3 Desc"].append(raw_text[i])
         del raw_text[0:col_len]
@@ -164,20 +161,58 @@ for page in range(10):
             story_datasets["Sales £"].append(raw_text[i])
         del raw_text[0:col_len]
 
-        #append last list to Units
+        # append last list to Units
         for i in range(len(raw_text)):
             story_datasets["Units"].append(raw_text[i])
 
-        #for key in story_datasets:
-        #    print("{} | {} \n".format(key,story_datasets[key]))
-
-
-    #elif raw_text[0] == "Item L3 Desc" and wtd is True:
-    #    print("Triggered")
-    #    # do cat stuff
     else:
         pass
 
-for dataset in store_datasets:
-    df = pd.DataFrame.from_dict(dataset)
-    df.to_excel("test.xlsx", engine='xlsxwriter')
+# datasets to df -> to excel
+with pd.ExcelWriter('test.xlsx') as writer:
+    for dataset in store_datasets:
+        sheetname = dataset["store"][0]
+        df = pd.DataFrame.from_dict(dataset)
+        df.set_index(["store", "STORY", "Item L3 Desc"], inplace=True)
+        df.to_excel(writer, engine='xlsxwriter', sheet_name=sheetname)
+
+    # collate store, ks, inno to df
+    ks_data = []
+    inno_data = []
+    solus_data = []
+    for dataset in store_datasets:
+        if "Karstadt" in dataset["store"][0]:
+            dataset["Grouping"] = ["Karstadt" for x in range(len(dataset["store"]))]
+            del dataset["store"]
+            df = pd.DataFrame.from_dict(dataset)
+            df.set_index(["Grouping", "STORY", "Item L3 Desc"], inplace=True)
+            ks_data.append(df)
+        elif "Inno" in dataset["store"][0]:
+            dataset["Grouping"] = ["Inno" for x in range(len(dataset["store"]))]
+            del dataset["store"]
+            df = pd.DataFrame.from_dict(dataset)
+            df.set_index(["Grouping", "STORY", "Item L3 Desc"], inplace=True)
+            inno_data.append(df)
+        elif ("Inno" not in dataset["store"][0]) and ("Karstadt" not in dataset["store"][0]) and (dataset["store"][0] != "INTERNATIONAL"):
+            dataset["Grouping"] = ["Solus" for x in range(len(dataset["store"]))]
+            del dataset["store"]
+            df = pd.DataFrame.from_dict(dataset)
+            df.set_index(["Grouping", "STORY", "Item L3 Desc"], inplace=True)
+            solus_data.append(df)
+    
+    collated_dfs = []
+    collated_dfs.append(ks_data)
+    collated_dfs.append(inno_data)
+    collated_dfs.append(solus_data)
+    for dfs in collated_dfs:
+        collated_df = pd.concat(dfs)
+        sheetname = collated_df.index.get_level_values(0)[0]
+        collated_df.groupby(level=[1,2])
+        collated_df.to_excel(writer, engine='xlsxwriter', sheet_name=sheetname)
+
+    
+
+
+
+
+
